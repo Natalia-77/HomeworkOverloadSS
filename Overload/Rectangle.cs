586e63b298b8a42bef65cc57c36bc8fbc07602bd
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-10-05 19:42:43 +0300

    Add classes

---
+++ Overload/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Overload
+{
+    class Rectangle
+    {
+        public string Name { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Rectangle( string name, double width,double height)
+        {
+            Width = width;
+            Height = height;
+            Name = name;
+        }
+
+        public double Area()
+        {
+            return Width*Height;
+        }
+
+        public double Perimeter()
+        {
+            return (Width+Height)*2;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Figure {Name} has area: {Area()} and perimeter {Perimeter()}");
+        }
+
+    }
+}
---
+++ Overload/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Overload
+{
+    class Square
+    {
+        public string NameFigure { get; set; }
+        public double SideLength { get; set; }
+
+        public Square(string name, double sidelength)
+        {
+            SideLength = sidelength;
+            NameFigure = name;
+        }
+
+        public double Area()
+        {
+            return SideLength * SideLength;
+        }
+
+        public double Perimeter()
+        {
+            return SideLength * 4;
+        }        
+
+        public void Show()
+        {
+            Console.WriteLine($"Figure {NameFigure} has area: {Area()} and perimeter {Perimeter()}");
+        }
+    }
+}
